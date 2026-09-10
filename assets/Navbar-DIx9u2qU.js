66bd6768f9e0eb9f1d426745107b28ec70daad63
piyusheris-9 <piyushtripathi@Piyushs-MacBook-Air.local>
2026-09-11 01:35:09 +0530

    Updates

--- assets/Navbar-DIx9u2qU.js
+++ assets/Navbar-DIx9u2qU.js
@@ -1,4 +1,4 @@
-import{j as A,r as yt}from"./index-LfCKLnta.js";import{g as St,S as ct}from"./ScrollTrigger-CiEuWA-R.js";/*!
+import{j as A,r as yt}from"./index-Br58tHUa.js";import{g as St,S as ct}from"./ScrollTrigger-CiEuWA-R.js";/*!
  * SplitText 3.15.0
  * https://gsap.com
  *
